4fe844e4837385a25772229566a381c3998eef71
aravhegde478 <aravhegde.261ch010@nitk.edu.in>
2026-08-28 01:11:20 +0530

    Add files via upload

---
+++ pset0-DeepThought.py
@@ -0,0 +1,9 @@
+x = input ("What is the answer to the great question of life, the universe, and everything? ")
+if x == "forty two" or x == "42":
+    print ("Yes")
+
+elif x == "forty-two":
+    print ("Yes")
+else:
+    print ("No")
+
